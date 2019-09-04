950e1ca38ee2f180a6620e3bb9f7f8ef9594a08f
tanisha2398 <tanishanegipro@gmail.com>
2019-09-04 15:51:03 +0530

    add posts container

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,48 +1,11 @@
 import React, { Component } from "react";
-import axios from "../../axios";
+// import axios from "../../axios";
+import Posts from "./Posts/Posts";
 
 import "./Blog.css";
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false
-  };
-
-  componentDidMount() {
-    axios
-      .get("/posts")
-      .then(response => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map(post => {
-          return { ...post, author: "Tanisha" };
-        });
-        this.setState({ posts: updatedPosts });
-        console.log(response);
-      })
-      .catch(error => {
-        this.setState({ error: true });
-      });
-  }
-  postSelectedHandler = id => {
-    this.setState({ selectedPostId: id });
-  };
   render() {
-    let posts = <p style={{ textAlign: "center" }}>Something went wrong!</p>;
-    if (!this.state.error) {
-      posts = this.state.posts.map(post => {
-        return (
-          <Post
-            title={post.title}
-            key={post.id}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}
-          />
-        );
-      });
-    }
-
     return (
       <div className="Blog">
         <header>
@@ -57,7 +20,7 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <section className="Posts">{posts}</section>
+        <Posts />
       </div>
     );
   }
