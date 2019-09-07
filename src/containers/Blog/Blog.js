1e2705deb632a8ba533d33257f293a6bfb1161bc
tanisha2398 <tanishanegipro@gmail.com>
2019-09-07 12:50:39 +0530

    nested route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import { Route, NavLink, Switch } from "react-router-dom";
 // import axios from "../../axios";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
-import FullPost from "./FullPost/FullPost";
+
 import "./Blog.css";
 
 class Blog extends Component {
@@ -15,7 +15,7 @@ class Blog extends Component {
             <ul>
               <li>
                 <NavLink
-                  to="/"
+                  to="/posts/"
                   exact
                   activeClassName="my-active"
                   activeStyle={{
@@ -43,9 +43,8 @@ class Blog extends Component {
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
     <Route path="/" render={() => <h1>Home2</h1>} />*/}
         <Switch>
-          <Route path="/" exact component={Posts} />
           <Route path="/new-post" component={NewPost} />
-          <Route path="/:id" component={FullPost} />
+          <Route path="/posts" component={Posts} />
         </Switch>
       </div>
     );
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import axios from "../../../axios";
 import Post from "../../../components/Post/Post";
-import { Link } from "react-router-dom";
+import { Route } from "react-router-dom";
+import FullPost from "../FullPost/FullPost";
 import "./Posts.css";
 class Posts extends Component {
   state = {
@@ -26,7 +27,7 @@ class Posts extends Component {
   }
   postSelectedHandler = id => {
     // this.setState({ selectedPostId: id });
-    this.props.history.push("/" + id);
+    this.props.history.push("/posts/" + id);
   };
   render() {
     let posts = <p style={{ textAlign: "center" }}>Something went wrong!</p>;
@@ -44,7 +45,16 @@ class Posts extends Component {
         );
       });
     }
-    return <section className="Posts">{posts}</section>;
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <Route
+          path={this.props.match.url + "/:id"}
+          exact
+          component={FullPost}
+        />
+      </div>
+    );
   }
 }
 export default Posts;
