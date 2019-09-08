33a1031aee45949b3f18d6575099d718574874d8
tanisha2398 <tanishanegipro@gmail.com>
2019-09-08 11:55:36 +0530

    add dynamic nested routes

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -8,12 +8,19 @@ class FullPost extends Component {
   };
   componentDidMount() {
     console.log(this.props);
+    this.loadData();
+  }
+  componentDidUpdate() {
+    this.loadData();
+  }
+  loadData() {
     if (this.props.match.params.id) {
       if (
         !this.state.loadedPost ||
         (this.state.loadedPost &&
-          this.state.loadedPost.id !== this.props.match.params.id)
-      ) {
+          this.state.loadedPost.id !==  +this.props.match.params.id)
+      ) { 
+
         axios.get("/posts/" + this.props.match.params.id).then(response => {
           this.setState({ loadedPost: response.data });
           console.log(response);
@@ -22,7 +29,7 @@ class FullPost extends Component {
     }
   }
   deletePostHandler = () => {
-    axios.delete("/posts/" + this.props.id).then(response => {
+    axios.delete("/posts/" + this.props.match.params.id).then(response => {
       console.log(response);
     });
   };
